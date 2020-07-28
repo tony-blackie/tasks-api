a9c2ece8d6d00ff841982bf338519f8530929b8d
Tony Spaskyi <jim.casey321@gmail.com>
2020-07-28 09:50:18 +0300

    add prod db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TYPE_ORM_CONFIG } from './config/typeorm.config';
+import { DB_CONFIG_LOCAL, DB_CONFIG_PROD } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 
+const dbConfig =
+    process.env.NODE_ENV === 'development' ? DB_CONFIG_LOCAL : DB_CONFIG_PROD;
+
 @Module({
-    imports: [TypeOrmModule.forRoot(TYPE_ORM_CONFIG), TasksModule, AuthModule],
+    imports: [TypeOrmModule.forRoot(dbConfig), TasksModule, AuthModule],
 })
 export class AppModule {}
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,24 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const TYPE_ORM_CONFIG: TypeOrmModuleOptions = {
+export const DB_CONFIG_LOCAL: TypeOrmModuleOptions = {
     type: 'postgres',
     host: 'localhost',
     port: 5432,
     username: 'postgres',
     password: 'asdfZxcv',
     database: 'taskmanagement',
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    synchronize: true,
+};
+
+export const DB_CONFIG_PROD: TypeOrmModuleOptions = {
+    type: 'postgres',
+    host: 'ec2-54-247-118-139.eu-west-1.compute.amazonaws.com',
+    port: 5432,
+    username: 'jvoflhhwonkswq',
+    password:
+        'f9d19882812f41cd08cbaf3d57c1cbdb0b809eed9db2948579450848a6c9f3ad',
+    database: 'd3jgvavu5ffrk1',
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true,
 };
